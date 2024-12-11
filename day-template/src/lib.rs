1d1cbeb402b0fe654e3f980969c30caa68c87b19
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2024-12-11 15:24:37 +0100

    use color-eyre not thiserror for aoc, skip author name

--- day-template/src/lib.rs
+++ day-template/src/lib.rs
@@ -1,18 +1,11 @@
 use aoclib::parse;
+use color_eyre::Result;
 use std::path::Path;
 
-pub fn part1(input: &Path) -> Result<(), Error> \{
+pub fn part1(input: &Path) -> Result<()> \{
     unimplemented!("input file: \{:?}", input)
 }
 
-pub fn part2(input: &Path) -> Result<(), Error> \{
+pub fn part2(input: &Path) -> Result<()> \{
     unimplemented!("input file: \{:?}", input)
-}
-
-#[derive(Debug, thiserror::Error)]
-pub enum Error \{
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-    #[error("no solution found")]
-    NoSolution,
 }
